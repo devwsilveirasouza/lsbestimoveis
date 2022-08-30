2d2a3288cdc1a523d38e99f42a56e2e9fb149bab
devwsilveirasouza <dev.wsilveirasouza@gmail.com>
2022-08-30 08:06:03 -0300

    Image

--- config/app.php
+++ config/app.php
@@ -162,9 +162,12 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+
         /*
          * Package Service Providers...
          */
+        // Intervention image
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -229,6 +232,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        // Intervention image
+        'Image' => Intervention\Image\Facades\Image::class,
 
     ],
 
